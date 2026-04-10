b939025b299a516ff1c3bbd3d01dcecbb8eb5edc
digitech.id <ibrabahalybrahim@gmail.com>
2026-04-10 13:48:24 +0700

    Add hobby variable and print statement to main function

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,7 +1,11 @@
 fun main() {
     val nama = "Ibrahim"
     val umur = 20
+    val Hobi = "Menulis"
+
 
     println("Halo $nama")
     println("Umur kamu $umur tahun")
+    println("Hobi kamu $Hobi")
+
 }
